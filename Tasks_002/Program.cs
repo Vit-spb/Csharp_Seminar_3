d5950639a8678af0958ecb5b689564cbac0589b5
Vit <wwg79@mail.ru>
2023-10-20 18:33:08 +0300

    ok

---
+++ Tasks_002/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+
+int result1 = (int)Math.Pow(pointA[0] - pointB[0], 2);
+int result2 = (int)Math.Pow(pointA[1] - pointB[1], 2);
+int result3 = (int)Math.Pow(pointA[2] - pointB[2], 2);
+double dist = Math.Sqrt(result1 + result2 + result3);
+return dist;
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
