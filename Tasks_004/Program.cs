5a480c861ed864ea728aa5e19e0d4c2b436b6748
Vit <wwg79@mail.ru>
2023-10-21 21:02:27 +0300

    ok

--- Tasks_004/Program.cs
+++ Tasks_004/Program.cs
@@ -1,11 +1,11 @@
 ﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
+
 int Expon(int numberA, int numberB)
 {
     int exp = 1;
     int numB = Math.Abs(numberB);
-    
 if (numberB !=0)
 {
    for(int i = 1; i <= numB; i++)
@@ -17,9 +17,7 @@ if (numberB !=0)
 else
      Console.Write("Число B не может равняться - ");
      return 0; 
-
 }
-
 int Number(string Num)
 {
     Console.WriteLine(Num);   
